0f75cbbdd528d729c22834963d25e96cbb1323c1
DoNaT <109744807+DoNaTzxc@users.noreply.github.com>
2022-07-26 20:52:56 +0300

    1

---
+++ module/rate/mrate.py
@@ -0,0 +1,58 @@
+
+def setup(bot):
+    import discord
+    import json
+    from discord.ext import commands
+
+    @bot.listen('on_message')
+    async def my_message(message):
+        try:
+            with open('users.json', 'r') as file:
+                data = json.load(file)
+                IgnoreChannels = data[str(message.guild.id)]['IgnoreChannels']
+                IgnoreRoles = data[str(message.guild.id)]['IgnoreRoles']
+            if not(message.content.startswith("~") or str(message.channel.id) in IgnoreChannels or True in [str(ii) in IgnoreRoles for ii in [i.id for i in message.author.roles]] or message.author.bot):
+
+                    xp = data[str(message.guild.id)]['USERS'][str(message.author.id)]['SCR']
+                    lvl = data[str(message.guild.id)]['USERS'][str(message.author.id)]['LvL']
+        
+                    if len(message.content) < 100:
+                        with open('users.json', 'w') as f:
+                            data[str(message.guild.id)]['USERS'][str(message.author.id)]['SCR'] = xp + 5 + (len(message.content))//10
+                            json.dump(data, f, indent=4)
+                    elif len(message.content) >= 100 and len(message.content) < 200:
+                        with open('users.json', 'w') as f:
+                            data[str(message.guild.id)]['USERS'][str(message.author.id)]['SCR'] = xp + 5 + (len(message.content))//40
+                            json.dump(data, f, indent=4)
+                    elif len(message.content) >= 200 and len(message.content) < 500:
+                        with open('users.json', 'w') as f:
+                            data[str(message.guild.id)]['USERS'][str(message.author.id)]['SCR'] = xp + 5 + (len(message.content))//50
+                            json.dump(data, f, indent=4)
+                    elif len(message.content) >= 500 and len(message.content) < 1000:
+                        with open('users.json', 'w') as f:
+                            data[str(message.guild.id)]['USERS'][str(message.author.id)]['SCR'] = xp + 5 + (len(message.content))//100
+                            json.dump(data, f, indent=4)
+                    elif len(message.content) >= 1000 and len(message.content) < 1500:
+                        with open('users.json', 'w') as f:
+                            data[str(message.guild.id)]['USERS'][str(message.author.id)]['SCR'] = xp + 5 + (len(message.content))//200
+                            json.dump(data, f, indent=4)
+                    elif len(message.content) >= 1500 and len(message.content) < 2000:
+                        with open('users.json', 'w') as f:
+                            data[str(message.guild.id)]['USERS'][str(message.author.id)]['SCR'] = xp + 5 + (len(message.content))//1000
+                            json.dump(data, f, indent=4)
+                    elif len(message.content) >= 2000:
+                        with open('users.json', 'w') as f:
+                            data[str(message.guild.id)]['USERS'][str(message.author.id)]['SCR'] = xp + 1
+                            json.dump(data, f, indent=4)
+
+                    increased_xp = xp+15
+                    new_level = int(increased_xp/100)
+                    if new_level > lvl:
+                        await message.channel.send(f"{message.author.mention} получил новый уровень {new_level}!")
+                        
+                        with open("users.json", "w") as f:
+                            data[str(message.guild.id)]['USERS'][str(message.author.id)]['SCR']=0
+                            data[str(message.guild.id)]['USERS'][str(message.author.id)]['LvL']=new_level
+                            json.dump(data, f, indent=4)
+        except:
+            pass
