bf99d526bcf5b60c489f7c9126a2070906e98398
stebo000000 <3367977@gmail.com>
2022-09-03 08:48:05 +0300

    wave

--- system/while.py
+++ system/while.py
@@ -1,13 +1,33 @@
 import time
 import asyncio
 import multiprocessing
+import os
 
 import discord_components
 from discord.ext import commands
 from discord_components import ComponentsBot
 
 
+class Wile():
+    def __init__(self, bot):
+        self.bot = bot
+
+    def res_copy(self):
+        #os.popen(f'copy {} ')
+        pass
+
+    def while_true(self):
+        while 1:
+            time.sleep(60)
+
+            Wile.res_copy()
+
+
 class Wile_on(commands.Cog):
     def __init__(self, bot):
         self.bot: discord_components.ComponentsBot = bot
 
+
+
+def setup(bot):
+    bot.add_cog(Wile_on(bot))
