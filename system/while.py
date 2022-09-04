b3c9024f136b638ec2fa99903dbe404e2aaae495
stebo000000 <3367977@gmail.com>
2022-09-04 10:03:49 +0300

    wave

--- system/while.py
+++ system/while.py
@@ -9,12 +9,12 @@ from discord_components import ComponentsBot
 import datetime
 
 def res_copy():
-    if not int(datetime.datetime.now().time().hour):
+    if not int(datetime.datetime.now().time().hour) and int(datetime.datetime.now().minute) <= 1:
         for i in os.listdir('.'):
             if i.endswith('.json'):
                 subprocess.call(fr'copy {i} system\rezerv\{i[:-5]}_rez.json', shell=True, stdout=subprocess.DEVNULL)
 
-def prnt():
+def prnt(bot):
     while 1:
         res_copy()
         time.sleep(60)
@@ -23,7 +23,7 @@ class Wile_on(commands.Cog):
     def __init__(self, bot):
         self.bot: discord_components.ComponentsBot = bot
 
-        pr = multiprocessing.Process(target=prnt)
+        pr = multiprocessing.Process(target=prnt, args=(bot,))
         pr.start()
 
 
