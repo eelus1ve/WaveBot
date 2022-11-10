ec9c56ec3e11d64ceb34b777b090c99602bcd359
DoNaT <109744807+DoNaTzxc@users.noreply.github.com>
2022-11-10 21:46:57 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   module/fun/8ball.py

--- module/fun/8ball.py
+++ module/fun/8ball.py
@@ -2,7 +2,6 @@ import discord
 import random
 from discord.ext import commands
 from BTSET import bdpy
-
 class Ballpy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -16,7 +15,5 @@ class Ballpy(commands.Cog):
             description="*Да!*" if randball == 1 else False or "*Может быть.*" if randball == 2 else False or "*Нет!*" if randball == 3 else False,
             color=COLOR
         ))
-            
-
 def setup(bot):
     bot.add_cog(Ballpy(bot))
