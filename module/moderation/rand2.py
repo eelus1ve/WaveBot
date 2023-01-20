748d2649caba260ed6cf45d9d87a915ee3ea02d9
stebo000000 <3367977@gmail.com>
2023-01-20 19:00:43 +0300

    wave

--- module/moderation/rand2.py
+++ module/moderation/rand2.py
@@ -38,46 +38,7 @@ class Selectpy(commands.Cog):
             COLOR = bdpy(ctx=interaction)['COLOR']
             ErCOLOR = bdpy(ctx=interaction)['ErCOLOR']
             roles = bdpy(ctx=interaction)['ROLES']
-            import json
-            with open(f'{BD}users.json', 'r') as file:
-                data = json.load(file)
-            #========================================Часть мейна=====================================================
-            if interaction.component.placeholder.startswith('Укажите'):
-                serverRoles = []
-                for i in range(0, len(interaction.guild.roles),
-                            24):  # первый важный комент !!!!!!!!!!!!!!!!!!!!!!!!! нужно при случаи ошибки заменить на 25!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                    serverRoles.append(interaction.guild.roles[
-                                    i:i + 24])  # второй важный комент !!!!!!!!!!!!!!!!!!!!!!!!! нужно при случаи ошибки заменить на 25!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-                if interaction.component.placeholder == 'Укажите классы в которые вы хотите добавить роли':
-                    for i in interaction.values:
-                        for rls in serverRoles:
-                            await interaction.send(embed=discord.Embed(
-                                title=f'Укажите роли которые вы хотите добавить в класс {i}',
-                                color=COLOR
-                            ),
-                            components=[
-                                Select(
-                                    placeholder=f'Укажите роли которые вы хотите добавить в класс *{i}',
-                                    max_values=len(rls),
-                                    min_values=0,
-                                    options=[Sel(label=i.name, value=i.id) for i in rls]
-                                )
-                                ]
-                            )
-
-                elif interaction.component.placeholder.startswith('Укажите роли которые вы хотите добавить в класс'):
-                    data[str(interaction.author.guild.id)]['ROLES'][interaction.component.placeholder.split('*')[1]][0] = interaction.values
-                    data[str(interaction.author.guild.id)]['ROLES'][interaction.component.placeholder.split('*')[1]][1] = [0 for i in interaction.values]
-                    await interaction.send(embed=discord.Embed(
-                        title=f'Роли выбранны',
-                        color=COLOR
-                    ))
-
-                with open(f'{BD}users.json', 'w') as file:
-                  json.dump(data, file, indent=4) 
-            #========================================================================================================
-            elif interaction.component.placeholder in [k for k in roles.keys()]:
+            if interaction.component.placeholder in [k for k in roles.keys()]:
                 a = interaction.author
                 try:
                     await interaction.send(embed=discord.Embed(
@@ -95,11 +56,6 @@ class Selectpy(commands.Cog):
 
                 try:
                     await a.remove_roles(*[interaction.guild.get_role(role_id=int(ii)) for ii in roles[str(interaction.component.placeholder)][0] if not(ii in interaction.values)])
-                except:
-                    pass
-                    
-                try:
-                    
                     await a.add_roles(*[interaction.guild.get_role(int(i)) for i in interaction.values])
                 except:
                     pass
