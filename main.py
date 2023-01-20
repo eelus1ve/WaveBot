d455be06aded02e451bc2d27568784756fe5d3e1
stebo000000 <3367977@gmail.com>
2023-01-20 19:16:43 +0300

    wave

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 #=============================================================================================импорты
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!       --> token стёпы <---             !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!      #нахуя?       #нужно было!     #нахуя?    #чтобы токен поменять!5
-import multiprocessing
-from discord_components import SelectOption as Sel
-import discord_components
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!       --> token стёпы <---             !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!      #нахуя?       #нужно было!     #нахуя?    #чтобы токен поменять! #ещё комент кста
 from discord.ext import commands
 from dotenv import load_dotenv, find_dotenv
-import interactions
-from discord_components import ComponentsBot, Button, Select
-from pydoc import cli
+from discord_components import ComponentsBot
 import discord
 import os
-import asyncio
 from BTSET import bdpy, embpy
-from interactions import TextInput, Modal, TextStyleType, SelectMenu, SelectOption, error
-import re
-
 
 load_dotenv(find_dotenv())
 
+intents = discord.Intents.all()
 
-#=======================================================================================================================
-intents=discord.Intents.all()
+
 def get_prefix(message):
     try:
         prefix = bdpy(ctx=message)['PREFIX']
@@ -28,39 +19,29 @@ def get_prefix(message):
         prefix = '~'
     return prefix
 
+
 def mention_and_prefix(bot, message):
     return commands.when_mentioned(bot, msg=message) + list(get_prefix(message))
-        
-client = interactions.Client(token=os.getenv('TOKEN'))
-bot =ComponentsBot(command_prefix = mention_and_prefix, intents=intents)
-bot.remove_command('help')
-#=======================================================================================================================
 
-#=======================================================================================================================
+
+bot =ComponentsBot(command_prefix=mention_and_prefix, intents=intents)
+bot.remove_command('help')
+
+
 @bot.event
 async def on_ready():
     bot.load_extension('module.loader')
     bot.load_extension('system.JSONwriter')
     bot.load_extension('system.while')
-
-    client.load('module.interactions.interactions')
-    client.reload('module.interactions.interactions')
-    
-
+    await bot.change_presence(activity=discord.Game('Portal 2'))
     print(f'{bot.user.name} connected')
 
-    await bot.change_presence(activity=discord.Game('Portal 2'))
-#=======================================================================================================================
 
-#=======================================================================================================================
 @bot.command()
 async def a(ctx: commands.Context):
-    client.load('module.interactions.interactions')
     await embpy(ctx, comp='s', des=f'Степа все плохо')
-
-#===================================================================================================
     
-#=======================================================================================================================
+
 # @bot.event
 # async def on_command_error(ctx, error):
 #     ErCOLOR = bdpy(ctx)['ErCOLOR']
@@ -83,24 +64,9 @@ async def a(ctx: commands.Context):
     # elif isinstance(error, commands.errors.CommandInvokeError):
     #     pass
 
-#=======================================================================================================================
-
-
-# @bot.event
-# async def on_error(ctx, err):
-#     print('ошибка', ':', ctx)
-#     print(err)
-
 
 def main():
-
-    loop = asyncio.get_event_loop()
-
-    task2 = loop.create_task(bot.start(os.getenv('TOKEN')))
-    task1 = loop.create_task(client._ready())
-
-    gathered = asyncio.gather(task1, task2, loop=loop)
-    loop.run_until_complete(gathered)
+    bot.run(os.getenv('TOKEN'))
 
 
 if __name__ == '__main__':
