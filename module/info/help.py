061b1a211779d8de188309f200240ef24183811f
stebo000000 <3367977@gmail.com>
2023-01-29 12:59:20 +0300

    wave

--- module/info/help.py
+++ module/info/help.py
@@ -1,82 +1,217 @@
 import discord
+import discord_components
 from discord.ext import commands
 from BTSET import BOTVERSION, bdpy
+from discord_components import Select, SelectOption, Button
+
+
 class Helppy(commands.Cog):
 
     help_dict = {
-                'rand': 'рандомайзер',
-                'info': 'Информация о боте',
-                'warn': 'Выдать предупреждение',
-                'unwarn': 'Убрать предупреждение',
-                'clear_warns': 'Очистка предупреждений',
-                'coin': 'Кинуть монетку',
-                'ban': 'Забанить пользователя',
-                'clear': 'Очистка чата',
-                'kick': 'Кикнуть пользователя',
-                'select': 'Выдача ролей',
-                'join': 'Присоединение бота к голосовому чату',
-                'play': 'Присоединение бота к голосовому чату',
-                'start': 'Присоединение бота к голосовому чату',
-                'stop': 'Присоединение бота к голосовому чату',
-                'pause': 'Присоединение бота к голосовому чату',
-                'spotify': 'Информация юзера о прослушивании spotify',
-                'user': 'Информация о юзере',
-                'MagicBall': 'Магический шар',
-                'server_info': 'Информация о сервере',
-                'mafia': 'Запустить мафию',
-                'score': 'осмотреть счет учасника',
-                'add_score': '',
-                'remove_score': '',
-                'clear_score': '',
-                'tr': 'Перевести текст',
-                'vote': 'Вызвать голосование',
-                'rank': 'Показать ваш ранк на сервере',
-                'settings': '***Параметры:*** \n\
-                                add_class (название класса): Добавить класс с ролей\n\
-                                remove_class (название класса): Удалить класс с ролями\n\
-                                add_role (id роли) (название класса): Добавить роль в класс\n\
-                                remove_role (id роли) (название класса): Удалить роль из класса\n\
-                                color (Ваш цвет в HEX): Цвет обычных сообщений бота \n\
-                                ercolor (Ваш цвет в HEX): Цвет сообщений с ошибками бота \n\
-                                IDА (ID Админ чата): Добавляет ID Админ чата \n\
-                                nCaps: Максимальное число капсов для предупреждения \n\
-                                nWarns: Максимальное число варнов до бана \n\
-                                add_badword (слово)\n\
-                                remove_badword (слово котороe нужно исключить из списка плохих слов) \n\
-                                selfroom (Id воис канала): Сделать комнату для создания личных каналов\n\
-                                selftitle (Текст): Текст, который будет указан при выборе ролей \n\
-                                join_message (Текмт): Изменить текст отправляемый учаснику при присоединении'
+        'модерация': {
+            'warn': 'Выдать предупреждение',
+            'unwarn': 'Убрать предупреждение',
+            'clear_warns': 'Очистка предупреждений',
+            'ban': 'Забанить пользователя',
+            'clear': 'Очистка чата',
+            'kick': 'Кикнуть пользователя',
+            'select': 'Выдача ролей',
+            'btst': 'Вызвать меню настройки бота',
+            'set': 'Настройка бота через чат'
+        },
+        'развлечения': {
+            'rand': 'рандомайзер',
+            'coin': 'Кинуть монетку',
+            'MagicBall': 'Магический шар',
+            'send_an_message': 'Отправить анонимное сообщение пользователю',
+            'p2048': 'Вызвать игру 2048',
+            'get_qr': 'зашифровать Ваш текст в QR код',
+            'dice': 'Кинуть игральные кости'
+        },
+        'информация': {
+            'info': 'Информация о боте',
+            'help': 'Информация о командах',
+            'user': 'Информация о юзере',
+            'spotify_info': '',
+            'spotify': 'Информация юзера о прослушивании spotify',
+            'server_info': 'Информация о сервере',
+            'server_info_channel': 'Создать каналы со статистикой сервера',
+        },
+        'рейтинг': {
+            'leaders': '',
+            'rank': 'Показать ваш ранк на сервере',
+            'add_score': '',
+            'remove_score': '',
+            'clear_score': '',
+            'score': 'осмотреть счет учасника',
+        },
+        'утилиты': {
+            'tr': 'Перевести текст',
+            'avatar': 'Отправить в чат аватар пользователя',
+            'vote': 'Вызвать голосование',
+        },
     }
 
     def __init__(self, bot):
         self.bot = bot
-    @commands.command(aliases = ['хелп', 'Хелп', 'ХЕЛП'])
-    async def help(self, ctx, *arg):
+
+    @commands.command(aliases=['хелп', 'Хелп', 'ХЕЛП'])
+    async def help(self, ctx, arg=None):
         COLOR = bdpy(ctx)['COLOR']
         pref = bdpy(ctx)['PREFIX']
-        if (arg[0] if arg else False):
+        if arg:
             await ctx.author.send(embed=discord.Embed(
-                title=f'***{arg[0]}***',
-                description=f"*Использование: {pref} {arg[0]}\n\
-                    {Helppy.help_dict[arg[0]]}*",
+                title=f'***{arg}***',
+                description=f"*Использование: {pref} {arg}\n\
+                    {[i[arg] for i in Helppy.help_dict if arg in i]}*",
                 color=COLOR
             ))
             await ctx.send(embed=discord.Embed(
                 title='Успешно',
-                description=f'Информация о комманде {arg[0]} отправлена вам в личные сообщения!',
+                description=f'Информация о комманде {arg} отправлена вам в личные сообщения!',
                 color=COLOR
             ))
 
         else:
+            var = list(Helppy.help_dict[list(Helppy.help_dict.keys())[0]].keys())
             emb = discord.Embed(title='*Список доступных команд:*',
-            description = f'\n'.join([i for i in Helppy.help_dict]),
-            color=COLOR)
-            await ctx.author.send(embed=emb)
+                description = '',
+                color=COLOR)
+            emb.add_field(name='', value='**`модерация`**' + '\n' + f'\n'.join([i for i in Helppy.help_dict if i != 'модерация']))
+            emb.add_field(name='', value=f'**`{var[0]}`**' + '\n' + f'\n'.join([i for i in var if i != var[0]]))
+            emb.add_field(name='='*len(Helppy.help_dict['модерация']['warn']), value=f"{Helppy.help_dict['модерация']['warn']}", inline=True)
+            await ctx.author.send(embed=emb, components=[[
+                Button(label='←'),
+                Button(label='↑'),
+                Button(label='→'),
+            ], [
+                Button(label='.', disabled=True),
+                Button(label='↓'),
+                Button(label='.', disabled=True),
+            ]])
             await ctx.send(embed=discord.Embed(
                 title='Успешно',
                 description=f'Список доступных комманд отправлен вам в личные сообщения!',
                 color=COLOR
             ))
+
+    @commands.Cog.listener('on_button_click')
+    async def help_scroll(self, interaction: discord_components.Interaction):
+        COLOR = 0x0000FF
+        pref = '~'
+
+        if interaction.component.label == '→':
+            old_field0_value = interaction.message.embeds[0].fields[0].value
+            old_field1_value = interaction.message.embeds[0].fields[1].value.split()
+
+            for i in range(len(old_field1_value)):
+                if '*' in old_field1_value[i]:
+                    old_field1_value[i] = old_field1_value[i].replace('*', '')
+                    old_field1_value[i] = old_field1_value[i].replace('`', '')
+                    if i < len(old_field1_value) - 1:
+                        des_name = old_field1_value[i+1]
+                        old_field1_value[i+1] = f'**`{old_field1_value[i+1]}`**'
+                    else:
+                        des_name = old_field1_value[0]
+                        old_field1_value[0] = f'**`{old_field1_value[0]}`**'
+                    break
+
+            for i in old_field0_value.split():
+                if '*' in i:
+                    first_key = i.replace('*', '').replace('`', '')
+
+            emb = discord.Embed(title='*Список доступных команд:*',
+                                description='',
+                                color=COLOR)
+
+            emb.add_field(name='', value=old_field0_value)
+            emb.add_field(name='', value=f'\n'.join(old_field1_value))
+            emb.add_field(name='='*len(Helppy.help_dict[first_key][des_name]), value=f'{Helppy.help_dict[first_key][des_name]}', inline=True)
+
+            await interaction.message.edit(embed=emb)
+            await interaction.edit_origin()
+
+        if interaction.component.label == '←':
+            old_field0_value = interaction.message.embeds[0].fields[0].value
+            old_field1_value = interaction.message.embeds[0].fields[1].value.split()
+
+            for i in range(len(old_field1_value)):
+                if '*' in old_field1_value[i]:
+                    old_field1_value[i] = old_field1_value[i].replace('*', '')
+                    old_field1_value[i] = old_field1_value[i].replace('`', '')
+                    des_name = old_field1_value[i - 1]
+                    old_field1_value[i - 1] = f'**`{old_field1_value[i - 1]}`**'
+                    break
+
+            for i in old_field0_value.split():
+                if '*' in i:
+                    first_key = i.replace('*', '').replace('`', '')
+
+            emb = discord.Embed(title='*Список доступных команд:*',
+                                description='',
+                                color=COLOR)
+
+            emb.add_field(name='', value=old_field0_value)
+            emb.add_field(name='', value=f'\n'.join(old_field1_value))
+            emb.add_field(name='=' * len(Helppy.help_dict[first_key][des_name]),
+                          value=f'{Helppy.help_dict[first_key][des_name]}', inline=True)
+
+            await interaction.message.edit(embed=emb)
+            await interaction.edit_origin()
+        
+        if interaction.component.label == '↑':
+            old_field0_value = interaction.message.embeds[0].fields[0].value.split()
+
+            for i in range(len(old_field0_value)):
+                if '*' in old_field0_value[i]:
+                    old_field0_value[i] = old_field0_value[i].replace('*', '')
+                    old_field0_value[i] = old_field0_value[i].replace('`', '')
+                    first_key = old_field0_value[i-1]
+                    old_field0_value[i-1] = f'**`{old_field0_value[i-1]}`**'
+                    break
+
+            var = list(Helppy.help_dict[first_key].keys())
+
+            emb = discord.Embed(title='*Список доступных команд:*',
+                                description='',
+                                color=COLOR)
+
+            emb.add_field(name='', value='\n'.join(old_field0_value))
+            emb.add_field(name='', value=f'**`{var[0]}`**' + '\n' + f'\n'.join([i for i in var if i != var[0]]))
+            emb.add_field(name='='*len(Helppy.help_dict[first_key][var[0]]), value=f'{Helppy.help_dict[first_key][var[0]]}', inline=True)
+
+            await interaction.message.edit(embed=emb)
+            await interaction.edit_origin()
+            
+        if interaction.component.label == '↓':
+            old_field0_value = interaction.message.embeds[0].fields[0].value.split()
+
+            for i in range(len(old_field0_value)):
+                if '*' in old_field0_value[i]:
+                    old_field0_value[i] = old_field0_value[i].replace('*', '')
+                    old_field0_value[i] = old_field0_value[i].replace('`', '')
+                    if i < len(old_field0_value) - 1:
+                        first_key = old_field0_value[i + 1]
+                        old_field0_value[i + 1] = f'**`{old_field0_value[i + 1]}`**'
+                    else:
+                        first_key = old_field0_value[0]
+                        old_field0_value[0] = f'**`{old_field0_value[0]}`**'
+                    break
+
+            var = list(Helppy.help_dict[first_key].keys())
+
+            emb = discord.Embed(title='*Список доступных команд:*',
+                                description='',
+                                color=COLOR)
+
+            emb.add_field(name='', value='\n'.join(old_field0_value))
+            emb.add_field(name='', value=f'**`{var[0]}`**' + '\n' + f'\n'.join([i for i in var if i != var[0]]))
+            emb.add_field(name='=' * len(Helppy.help_dict[first_key][var[0]]),
+                          value=f'{Helppy.help_dict[first_key][var[0]]}', inline=True)
+
+            await interaction.message.edit(embed=emb)
+            await interaction.edit_origin()
+
 
 def setup(bot):
     bot.add_cog(Helppy(bot))
