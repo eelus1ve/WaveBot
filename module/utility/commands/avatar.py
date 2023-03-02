9a1f74e14eaedc740ff7d7aaf35964e6e5a1217c
DoNaT <109744807+DoNaTzxc@users.noreply.github.com>
2023-03-03 01:52:03 +0300

    1

--- module/utility/commands/avatar.py
+++ module/utility/commands/avatar.py
@@ -1,12 +1,11 @@
 import discord
 from discord.ext import commands
-from typing import Optional
-from BTSET import Utility
+from BTSET import Utility, Lang
 class Avatar(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     async def command_avatar(self, ctx: commands.Context, member: discord.Member):
-        emb = discord.Embed(title=f'Аватар {member.name}', color=Utility(ctx).color)
+        emb = discord.Embed(title=f"{Lang(ctx).language['avatar_title']} {member.name}", color=Utility(ctx).color)
         emb.set_image(url=member.avatar_url)
         await ctx.send(embed=emb)
