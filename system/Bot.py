235652b65ebfaae093300269df97fd52ebd57fc8
stebo000000 <3367977@gmail.com>
2023-03-03 16:28:14 +0300

    wave

--- system/Bot.py
+++ system/Bot.py
@@ -9,12 +9,22 @@ from discord.ext.commands.help import HelpCommand
 
 _default: Any = _DefaultRepr()
 
-def schitat_ycherbstvo(function_to_decorate):
-    def the_wrapper(data, ctx):
-        with open(f'{BD}users.json', 'r') as file:
-            data =  json.load(file)
-        return function_to_decorate(data, ctx)
 
+def schitat_ycherbstvo(function_to_decorate):
+    def the_wrapper(self, data, ctx):
+        with open(f'{BD}users.json', 'r') as file:
+            data = json.load(file)
+        return function_to_decorate(self, data, ctx)
+    return the_wrapper
+
+
+def db_write(function_to_decorate):
+    def the_wrapper(self, data, ctx):
+        with open(f'{BD}users.json', 'r') as file:
+            data = json.load(file)
+        data = function_to_decorate(self, data, ctx)
+        with open(f'{BD}users.json', 'w') as file:
+            json.dump(data, file, indent=4)
     return the_wrapper
 
 
@@ -38,7 +48,110 @@ class WaveBot(commands.Bot):
         )
 
     @schitat_ycherbstvo
-    def db_get_check(data, ctx):
+    def db_get_lang(self, data, ctx):
         return data[str(ctx.guild.id)]['LANG']
 
-    
+    @schitat_ycherbstvo
+    def db_get_color(self, data, ctx):
+        return int(data[str(ctx.guild.id)]['COLOR'], 16)
+
+    @schitat_ycherbstvo
+    def db_get_ercolor(self, data, ctx):
+        return int(data[str(ctx.guild.id)]['ERCOLOR'], 16)
+
+    @schitat_ycherbstvo
+    def db_get_joinroles(self, data, ctx):
+        return data[str(ctx.guild.id)]['JoinRoles']
+
+    @schitat_ycherbstvo
+    def db_get_modroles(self, data, ctx):
+        return data[str(ctx.guild.id)]['ModRoles']
+
+    @schitat_ycherbstvo
+    def db_get_roles(self, data, ctx):
+        return data[str(ctx.guild.id)]['ROLES']
+
+    @schitat_ycherbstvo
+    def db_get_firstrole(self, data, ctx):
+        return data[str(ctx.guild.id)]['FirstRole']
+
+    @schitat_ycherbstvo
+    def db_get_actmoduls(self, data, ctx):
+        return data[str(ctx.guild.id)]['actmoduls']
+
+    @schitat_ycherbstvo
+    def db_get_ncaps(self, data, ctx):
+        return data[str(ctx.guild.id)]['NCAPS']
+
+    @schitat_ycherbstvo
+    def db_get_nwarns(self, data, ctx):
+        return data[str(ctx.guild.id)]['NWARNS']
+
+    @schitat_ycherbstvo
+    def db_get_adminchannel(self, data, ctx):
+        return data[str(ctx.guild.id)]['ADMINCHANNEL']
+
+    @schitat_ycherbstvo
+    def db_get_idmainch(self, data, ctx):
+        return data[str(ctx.guild.id)]['idMainch']
+
+    @schitat_ycherbstvo
+    def db_get_selfroom(self, data, ctx):
+        return data[str(ctx.guild.id)]['SELFROOM']
+
+    @schitat_ycherbstvo
+    def db_get_badwords(self, data, ctx):
+        return data[str(ctx.guild.id)]['BADWORDS']
+
+    @schitat_ycherbstvo
+    def db_get_links(self, data, ctx):
+        return data[str(ctx.guild.id)]['LINKS']
+
+    @schitat_ycherbstvo
+    def db_get_prefix(self, data, ctx):
+        return data[str(ctx.guild.id)]['PREFIX']
+
+    @schitat_ycherbstvo
+    def db_get_jnmsg(self, data, ctx):
+        return data[str(ctx.guild.id)]['JNMSG']
+
+    @schitat_ycherbstvo
+    def db_get_selftitle(self, data, ctx):
+        return data[str(ctx.guild.id)]['SelfTitle']
+
+    @schitat_ycherbstvo
+    def db_get_selfrooms(self, data, ctx):
+        return data[str(ctx.guild.id)]['Selfrooms']
+
+    @schitat_ycherbstvo
+    def db_get_mafrooms(self, data, ctx):
+        return data[str(ctx.guild.id)]['Mafrooms']
+
+    @schitat_ycherbstvo
+    def db_get_ignorechannels(self, data, ctx):
+        return data[str(ctx.guild.id)]['IgnoreChannels']
+
+    @schitat_ycherbstvo
+    def db_get_ignoreroles(self, data, ctx):
+        return data[str(ctx.guild.id)]['IgnoreRoles']
+
+    @schitat_ycherbstvo
+    def db_get_card(self, data, ctx):
+        return data[str(ctx.guild.id)]['card']
+
+    @schitat_ycherbstvo
+    def db_get_textcolor(self, data, ctx):
+        return data[str(ctx.guild.id)]['text_color']
+
+    @schitat_ycherbstvo
+    def db_get_barcolor(self, data, ctx):
+        return data[str(ctx.guild.id)]['bar_color']
+
+    @schitat_ycherbstvo
+    def db_get_blend(self, data, ctx):
+        return data[str(ctx.guild.id)]['blend']
+
+    @schitat_ycherbstvo
+    def db_get_users(self, data, ctx):
+        return data[str(ctx.guild.id)]['USERS']
+
