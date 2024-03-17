592794abb2660f098a7949c7f5e41d1ee9f93e8d
DoNaT <109744807+eelus1ve@users.noreply.github.com>
2024-03-18 00:27:49 +0300

    очередной ненужный комит

--- system/languages/language.py
+++ system/languages/language.py
@@ -1,22 +1,37 @@
-from typing import Union
-import discord
-from discord.ext import commands
+# from typing import Union
+# import discord
+# from discord.ext import commands
 import os
 
-class Lang:
-    def __init__(self, ctx: Union[commands.Context, discord.Interaction]):
-        self.language = self.lang(ctx)
 
-    def lang(self, ctx: commands.Context):
-        # sql = sqlite3.connect(f'{BD}WaveDateBase.db').cursor().execute(f"""SELECT LANG from servers WHERE ID == {ctx.guild.id}""").fetchone()[0]
+
+
+
+
+
+
+
+class Lang():
+    def __init__(self, ctx: Union[commands.Context, discord.Interaction]):
+        self.language = self.lang()
+
+    def get_lang(self, ctx: commands.Context):
         lang_dict = {}
-        part = 'system\\Languages\\ru.wave'     #потом заменить на en-US
-        # if os.path.exists('system\\Languages\\{}.wave'.format(str(ctx.guild.preferred_locale) if not sql else sql)):
-        #     part = 'system\\Languages\\{}.wave'.format(str(ctx.guild.preferred_locale) if not sql else sql)
-        for i in os.path.
-            with open(f'{part}', 'r', encoding='utf-8') as f:
-                for line in f:
-                    if not (line.startswith('//')) and not (line == '\n'):
-                        key, *value = line.split()
-                        lang_dict[key] = ' '.join([i.replace('\\n', '\n') for i in value])
-        return lang_dict
+		path = "system\\Languages\\"
+		lang_name = ""
+		return self.language[lang_name]
+    
+	@staticmethod
+    def lang(ctx: commands.Context):
+        lang_dict = {}
+		path = "system\\Languages\\"
+		for i in os.listdir(path):
+			if i == "language.py":
+				continue
+			lang_dict[i[:-4]] = {}
+			with open(f'{part+i}', 'r', encoding='utf-8') as f:
+				for line in f:
+					if not (line.startswith('//')) and not (line == '\n'):
+						key, *value = line.split()
+						lang_dict[key] = ' '.join([i.replace('\\n', '\n') for i in value])
+			return lang_dict
